0422252be311e567416d4c47b7878db841b6ed10
Carlos Vinicius <vinicius26092000@gmail.com>
2020-10-08 12:23:58 -0300

    added cors on server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import { createConnection } from 'typeorm';
 import { router } from './Routes';
 
 const app = express();
 
 createConnection().then(() => {
+  app.use(cors())
   app.use(express.json());
   app.use(helmet());
   app.use(router);
